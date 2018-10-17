63090d341dcddf8666f2284dedfffda088364553
Nunny Reyes <nunnyreyes@gmail.com>
2018-10-17 19:52:57 -0400

    next up randomize deck

--- js/app.js
+++ js/app.js
@@ -36,3 +36,60 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+const deck = document.querySelector('.deck');
+let toggledCards = []
+
+
+deck.addEventListener('click', event => {
+    const clickTarget = event.target;
+    if(clickTarget.classList.contains('card') && toggledCards.length < 2 && !toggledCards.includes(clickTarget)
+    ) {
+        toggleCard(clickTarget);
+        addToggleCard(clickTarget);
+        if(toggledCards.length === 2) {
+        checkForMatch(clickTarget);
+        console.log("2 cards")
+        }
+    }
+})
+
+
+
+function toggleCard(card) {
+    card.classList.toggle('open');
+    card.classList.toggle('show');
+}
+
+function addToggleCard(clickTarget) {
+    toggledCards.push(clickTarget);
+    console.log(toggledCards);
+}
+
+function checkForMatch() {
+    if (
+        toggledCards[0].firstElementChild.className === 
+        toggledCards[1].firstElementChild.className
+        ) {
+            toggledCards[0].classList.toggle('match');
+            toggledCards[1].classList.toggle('match');
+            toggledCards = [];
+            console.log('Match!');
+        } else {
+            setTimeout(() => {
+        
+            
+            toggleCard(toggledCards[0]);
+            toggleCard(toggledCards[1]);
+            toggledCards = [];
+        }, 1000);
+    }
+}
+
+function isClickValid(clickTarget) {
+    return (
+        clickTarget.classList.contains('card') && 
+        !clickTarget.classList.contains('match') && 
+        toggledCards.length < 2 && 
+        !toggledCards.includes(clickTarget)
+     );
+ }
